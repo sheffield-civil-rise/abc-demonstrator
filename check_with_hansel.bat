1d3cc355cd716e696bb7151e75748ef08b692784
tomhosker <tomdothosker@gmail.com>
2022-02-01 17:55:30 +0000

    Added relative paths to the .bat file.

--- check_with_hansel.bat
+++ check_with_hansel.bat
@@ -1,5 +1,5 @@
 :: This code defines a script which checks whether the code is running properly
 :: on Hansel.
 
-git pull
-python run_demonstrator.py
+git -C "%~dp0" pull
+python "%~dp0\run_demonstrator.py"
