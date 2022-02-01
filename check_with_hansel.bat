a4ef45ca399a3ea9f1a871145bd15f14769de430
tomhosker <tomdothosker@gmail.com>
2022-02-01 18:52:13 +0000

    Revised conda activation.

--- check_with_hansel.bat
+++ check_with_hansel.bat
@@ -1,6 +1,6 @@
 :: This code defines a script which checks whether the code is running properly
 :: on Hansel.
 
-conda activate demonstrator
-git -C %~dp0 pull
+::git -C %~dp0 pull
+C:\Users\hansel\Anaconda3\Scripts\activate demonstrator
 python "%~dp0\run_demonstrator.py"
