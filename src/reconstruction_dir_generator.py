9cba64ec9428ece5d887e3f9836fafe98c06250f
tomhosker <tomdothosker@gmail.com>
2022-02-08 19:08:20 +0000

    Debugging...

--- src/reconstruction_dir_generator.py
+++ src/reconstruction_dir_generator.py
@@ -375,6 +375,7 @@ class ReconstructionDirGenerator:
                     os.path.splitext(os.path.split(path)[-1])[0]
                     +self.output_image_extension
                 )
+            print(out_path)
             pad_im = \
                 cv2.copyMakeBorder(
                     bgr_mask,
@@ -389,7 +390,7 @@ class ReconstructionDirGenerator:
                     interpolation=cv2.INTER_NEAREST
                 )
             cv2.imwrite(self.path_to_labelled_images, out_im)
-            sys.stdout.write('\r%5d/%5d' % (index+1, len(img_list)))
+            sys.stdout.write("\r%5d/%5d"%(index+1, len(img_list)))
             sys.stdout.flush()
         print(" ")
 
