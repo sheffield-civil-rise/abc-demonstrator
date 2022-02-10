2c77279c31f8e8cf09cdfd4de9e6abcc205b3c09
tomhosker <tomdothosker@gmail.com>
2022-02-10 12:36:19 +0000

    Debugging...

--- src/demonstrator_new.py
+++ src/demonstrator_new.py
@@ -80,6 +80,7 @@ class Demonstrator:
     def demonstrate(self):
         """ Run the demonstrator script. """
         self.make_and_run_reconstruction_dir_generator()
+        self.run_batch_processes()
         print("THIS IS AS FAR AS THE SCRIPT SHOULD GET RIGHT NOW")
         sys.exit(0)
 
