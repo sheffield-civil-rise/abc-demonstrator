45651c5bcdbd80d1c400b70b07ca81fecfe7923d
tomhosker <tomdothosker@gmail.com>
2022-02-10 12:25:42 +0000

    Added method for running batch processes.

--- src/demonstrator_new.py
+++ src/demonstrator_new.py
@@ -33,10 +33,10 @@ class Demonstrator:
             path_to_output=config.DEFAULT_PATH_TO_DEMO_OUTPUT
         ):
         self.timeout = timeout # In seconds.
-        self.check_every = check_every
+        self.check_every = check_every # In seconds.
         self.path_to_output = path_to_output
         self.rec_dir_gen = None
-        self.batch_processor = None
+        self.path_to_cache = os.path.join(self.path_to_output, "cache")
 
     def make_and_run_reconstruction_dir_generator(self):
         """ Run the generator class, deleting any existing output as
@@ -47,25 +47,48 @@ class Demonstrator:
             ReconstructionDirGenerator(path_to_output=self.path_to_output)
         self.rec_dir_gen.generate()
 
+    def run_batch_processes():
+        camera_init_0 = \
+            os.path.join(
+                self.path_to_output, self.rec_dir_gen.CAMERA_INIT_FILENAME
+            )
+        camera_init_1 = \
+            os.path.join(
+                self.path_to_output, self.rec_dir_gen.CAMERA_INIT_LABEL_FILENAME
+            )
+        recon_thread_running = \
+            batch_process(
+                self.rec_dir_gen.path_to_output_images,
+                "custom",
+                cache=self.path_to_cache,
+                init=[camera_init_0, camera_init_1],
+                label_dir=self.rec_dir_gen.path_to_labelled_images
+            )
+        # Pause execution while photogrammetry running externally.
+        start_time = time.time()
+        while True:
+            if not recon_thread_running():
+                break
+            if (time.time()-start_time) > self.timeout:
+                raise DemonstratorException(
+                    "Timed out after "+str(self.timeout)+" seconds."
+                )
+            time.sleep(
+                self.check_every-((time.time()-start_time)%self.check_every)
+            )
+
     def demonstrate(self):
         """ Run the demonstrator script. """
         self.make_and_run_reconstruction_dir_generator()
         print("THIS IS AS FAR AS THE SCRIPT SHOULD GET RIGHT NOW")
         sys.exit(0)
 
-#############
-# FUNCTIONS #
-#############
+################################
+# HELPER CLASSES AND FUNCTIONS #
+################################
 
-#def run_batch_processes():
-#    recon_thread_running = \
-#        batch_process(
-#            image_dir,
-#            'custom',
-#            cache=cache_dir,
-#            init=[camera_init_0, camera_init_1],
-#            label_dir=label_dir
-#        )
+class DemonstratorException(Exception):
+    pass
 
 ###################
 # RUN AND WRAP UP #
