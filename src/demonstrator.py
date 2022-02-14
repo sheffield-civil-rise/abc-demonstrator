cf89ed9a4e03d4e98ccb37a83bee8b103871b803
tomhosker <tomdothosker@gmail.com>
2022-02-14 16:30:08 +0000

    Tidied up demonstrator class.

--- src/demonstrator.py
+++ src/demonstrator.py
@@ -13,7 +13,6 @@ import numpy
 
 # Local imports.
 import config
-from batch_process import run as batch_process
 from batch_processor import BatchProcessor
 from calculate_height import main as calculate_height
 from calculate_wwr import calculate as calculate_wwr
@@ -31,11 +30,13 @@ class Demonstrator:
             path_to_output=config.DEFAULT_PATH_TO_DEMO_OUTPUT
         ):
         self.path_to_output = path_to_output
-        self.rec_dir_gen = None
         self.path_to_cache = os.path.join(self.path_to_output, "cache")
+        # Generated fields.
+        self.rec_dir_gen = None
+        self.batch_processor = None
 
     def make_and_run_reconstruction_dir_generator(self):
-        """ Run the generator class, deleting any existing output as
+        """ Run the generator object, deleting any existing output as
         necessary. """
         if os.path.exists(self.path_to_output):
             shutil.rmtree(self.path_to_output)
@@ -43,7 +44,8 @@ class Demonstrator:
             ReconstructionDirGenerator(path_to_output=self.path_to_output)
         self.rec_dir_gen.generate()
 
-    def run_batch_processes(self):
+    def make_and_run_batch_processor(self):
+        """ Build the batch processor object, and then run it. """
         camera_init0 = \
             os.path.join(
                 self.path_to_output, self.rec_dir_gen.CAMERA_INIT_FILENAME
@@ -52,7 +54,7 @@ class Demonstrator:
             os.path.join(
                 self.path_to_output, self.rec_dir_gen.CAMERA_INIT_LABEL_FILENAME
             )
-        batch_pcssr = \
+        self.batch_processor = \
             BatchProcessor(
                 search_recursively=True,
                 path_to_output_images=self.rec_dir_gen.path_to_output_images,
@@ -61,7 +63,7 @@ class Demonstrator:
                 paths_to_init_files=[camera_init0, camera_init1],
                 path_to_labelled_images=self.rec_dir_gen.path_to_labelled_images
             )
-        batch_pcssr.run()
+        batch_processor.run()
 
     def demonstrate(self):
         """ Run the demonstrator script. """
@@ -69,13 +71,6 @@ class Demonstrator:
         self.run_batch_processes()
         print("THIS IS AS FAR AS THE SCRIPT SHOULD GET RIGHT NOW")
         sys.exit(0)
-
-################################
-# HELPER CLASSES AND FUNCTIONS #
-################################
-
-class DemonstratorException(Exception):
-    pass
 
 ###################
 # RUN AND WRAP UP #
