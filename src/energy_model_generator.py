910d64d869057e72a640e2d9212a29e5a43a7777
tomhosker <tomdothosker@gmail.com>
2022-02-17 22:12:36 +0000

    Debugging...

--- src/energy_model_generator.py
+++ src/energy_model_generator.py
@@ -283,7 +283,7 @@ class EnergyModelGenerator:
 
     def add_thermostat(self):
         """ Thermostat for HVAC control. """
-        self.thermostat = 
+        self.thermostat = \
             self.idf_obj.newidfobject(
                 "HVACTEMPLATE:THERMOSTAT",
                 Name="Zone Thermostat",
