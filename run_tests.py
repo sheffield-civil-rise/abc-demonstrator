64f27598e9710497a89161dff7f240d53642b29e
tomhosker <tomdothosker@gmail.com>
2022-02-21 17:54:36 +0000

    Debugging...

--- run_tests.py
+++ run_tests.py
@@ -15,7 +15,7 @@ HANSEL_PATH_TO_REPO = r"G:\photogrammetry_e110a"
 def run():
     """ Run this file. """
     try:
-        subprocess.run(["python", "-m", "pytest", HANSEL_PATH_TO_REPO], check=True)
+        subprocess.run(["pytest", HANSEL_PATH_TO_REPO], check=True)
     except subprocess.CalledProcessError:
         return False
     return True
