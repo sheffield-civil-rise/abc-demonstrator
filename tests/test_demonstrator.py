f56eabb3a8f323018cdd573bd3ad48c62b6d6bcf
tomhosker <tomdothosker@gmail.com>
2022-02-22 15:31:37 +0000

    Debugging...

--- tests/test_demonstrator.py
+++ tests/test_demonstrator.py
@@ -108,4 +108,6 @@ def test_energy_model_output(demo_obj):
     """ Test that the energy model has produced an output. """
     assert os.path.exists(demo_obj.energy_model_generator.path_to_output_idf)
     assert os.path.exists(demo_obj.energy_model_generator.path_to_output_dir)
-    assert os.listdir(demo_obj.energy_model_generator.path_to_output_dir) > 0
+    assert (
+        len(os.listdir(demo_obj.energy_model_generator.path_to_output_dir)) > 0
+    )
