0aa46d64cba8ec435e0c05d990b70f939eea7d60
tomhosker <tomdothosker@gmail.com>
2022-02-22 13:42:59 +0000

    Debugging...

--- tests/test_demonstrator.py
+++ tests/test_demonstrator.py
@@ -6,6 +6,9 @@ fields of sub-objects - of the Demonstrator class.
 # Standard imports.
 import os
 
+# Non-standard imports.
+import pytest
+
 # Local imports.
 import config
 import expected
@@ -16,138 +19,64 @@ from make_checksum import make_checksum
 # TESTING #
 ###########
 
-class TestDemonstrator:
-    """ A grouping of test methods. """
-    def __init__(self):
-        self.demo_obj = Demonstrator()
-        self.demo_obj.make_and_run_reconstruction_dir_generator()
+@pytest.fixture
+def demo_obj():
+    return Demonstrator()
 
-    def test_rec_dir_gen_fields(self):
-        """ Test the FIELDS of the ReconstructionDirGenerator sub-object. """
-        assert (
-            self.demo_obj.rec_dir_gen.path_to_output ==
-                config.DEFAULT_PATH_TO_DEMO_OUTPUT
-        )
-        assert (
-            self.demo_obj.rec_dir_gen.path_to_output_images ==
-                os.path.join(config.DEFAULT_PATH_TO_DEMO_OUTPUT, "images")
-        )
-        assert (
-            self.demo_obj.rec_dir_gen.path_to_labelled_images ==
-                os.path.join(config.DEFAULT_PATH_TO_DEMO_OUTPUT, "labelled")
-        )
-        assert (
-            self.demo_obj.rec_dir_gen.path_to_masked_images ==
-                os.path.join(config.DEFAULT_PATH_TO_DEMO_OUTPUT, "masked")
-        )
+def check_rec_dir_gen_fields(demo_obj):
+    """ Test the FIELDS of the ReconstructionDirGenerator sub-object. """
+    demo_obj.make_and_run_reconstruction_dir_generator()
+    assert (
+        demo_obj.rec_dir_gen.path_to_output ==
+            config.DEFAULT_PATH_TO_DEMO_OUTPUT
+    )
+    assert (
+        demo_obj.rec_dir_gen.path_to_output_images ==
+            os.path.join(config.DEFAULT_PATH_TO_DEMO_OUTPUT, "images")
+    )
+    assert (
+        demo_obj.rec_dir_gen.path_to_labelled_images ==
+            os.path.join(config.DEFAULT_PATH_TO_DEMO_OUTPUT, "labelled")
+    )
+    assert (
+        demo_obj.rec_dir_gen.path_to_masked_images ==
+            os.path.join(config.DEFAULT_PATH_TO_DEMO_OUTPUT, "masked")
+    )
 
-    def test_rec_dir_gen_files(self):
-        """ Test that the ReconstructionDirGenerator sub-object outputs the
-        right FILES. """
-        actual_labelled_image_checksum = \
-            make_checksum(
-                os.path.join(
-                    self.demo_obj.rec_dir_gen.path_to_labelled_images,
-                    expected.LABELLED_IMAGE_FILENAME
-                )
+def check_rec_dir_gen_files(demo_obj):
+    """ Test that the ReconstructionDirGenerator sub-object outputs the right
+    files. """
+    actual_labelled_image_checksum = \
+        make_checksum(
+            os.path.join(
+                demo_obj.rec_dir_gen.path_to_labelled_images,
+                expected.LABELLED_IMAGE_FILENAME
             )
-        actual_masked_image_checksum = \
-            make_checksum(
-                os.path.join(
-                    self.demo_obj.rec_dir_gen.path_to_masked_images,
-                    expected.MASKED_IMAGE_FILENAME
-                )
-            )
-        actual_camera_init_checksum = \
-            make_checksum(
-                os.path.join(
-                    self.demo_obj.rec_dir_gen.path_to_output, "cameraInit.sfm"
-                )
-            )
-        actual_camera_init_label_checksum = \
-            make_checksum(
-                os.path.join(
-                    self.demo_obj.rec_dir_gen.path_to_output, "cameraInit_label.sfm"
-                )
-            )
-        assert actual_labelled_image_checksum == expected.LABELLED_IMAGE_CHECKSUM
-        assert actual_masked_image_checksum == expected.MASKED_IMAGE_CHECKSUM
-        assert actual_camera_init_checksum == expected.CAMERA_INIT_CHECKSUM
-        assert (
-            actual_camera_init_label_checksum ==
-                expected.CAMERA_INIT_LABEL_CHECKSUM
         )
+    actual_masked_image_checksum = \
+        make_checksum(
+            os.path.join(
+                demo_obj.rec_dir_gen.path_to_masked_images,
+                expected.MASKED_IMAGE_FILENAME
+            )
+        )
+    actual_camera_init_checksum = \
+        make_checksum(
+            os.path.join(
+                demo_obj.rec_dir_gen.path_to_output, "cameraInit.sfm"
+            )
+        )
+    actual_camera_init_label_checksum = \
+        make_checksum(
+            os.path.join(
+                demo_obj.rec_dir_gen.path_to_output, "cameraInit_label.sfm"
+            )
+        )
+    assert actual_labelled_image_checksum == expected.LABELLED_IMAGE_CHECKSUM
+    assert actual_masked_image_checksum == expected.MASKED_IMAGE_CHECKSUM
+    assert actual_camera_init_checksum == expected.CAMERA_INIT_CHECKSUM
+    assert (
+        actual_camera_init_label_checksum ==
+            expected.CAMERA_INIT_LABEL_CHECKSUM
+    )
 
-#def check_rec_dir_gen_fields(demo_obj):
-#    """ Test the FIELDS of the ReconstructionDirGenerator sub-object. """
-#    assert (
-#        demo_obj.rec_dir_gen.path_to_output ==
-#            config.DEFAULT_PATH_TO_DEMO_OUTPUT
-#    )
-#    assert (
-#        demo_obj.rec_dir_gen.path_to_output_images ==
-#            os.path.join(config.DEFAULT_PATH_TO_DEMO_OUTPUT, "images")
-#    )
-#    assert (
-#        demo_obj.rec_dir_gen.path_to_labelled_images ==
-#            os.path.join(config.DEFAULT_PATH_TO_DEMO_OUTPUT, "labelled")
-#    )
-#    assert (
-#        demo_obj.rec_dir_gen.path_to_masked_images ==
-#            os.path.join(config.DEFAULT_PATH_TO_DEMO_OUTPUT, "masked")
-#    )
-
-#def check_rec_dir_gen_files(demo_obj):
-#    """ Test that the ReconstructionDirGenerator sub-object outputs the right
-#    files. """
-#    actual_labelled_image_checksum = \
-#        make_checksum(
-#            os.path.join(
-#                demo_obj.rec_dir_gen.path_to_labelled_images,
-#                expected.LABELLED_IMAGE_FILENAME
-#            )
-#        )
-#    actual_masked_image_checksum = \
-#        make_checksum(
-#            os.path.join(
-#                demo_obj.rec_dir_gen.path_to_masked_images,
-#                expected.MASKED_IMAGE_FILENAME
-#            )
-#        )
-#    actual_camera_init_checksum = \
-#        make_checksum(
-#            os.path.join(
-#                demo_obj.rec_dir_gen.path_to_output, "cameraInit.sfm"
-#            )
-#        )
-#    actual_camera_init_label_checksum = \
-#        make_checksum(
-#            os.path.join(
-#                demo_obj.rec_dir_gen.path_to_output, "cameraInit_label.sfm"
-#            )
-#        )
-#    assert actual_labelled_image_checksum == expected.LABELLED_IMAGE_CHECKSUM
-#    assert actual_masked_image_checksum == expected.MASKED_IMAGE_CHECKSUM
-#    assert actual_camera_init_checksum == expected.CAMERA_INIT_CHECKSUM
-#    assert (
-#        actual_camera_init_label_checksum ==
-#            expected.CAMERA_INIT_LABEL_CHECKSUM
-#    )
-
-#def test():
-#    """ Run the unit tests. """
-#    demo_obj = Demonstrator()
-#    demo_obj.make_and_run_reconstruction_dir_generator()
-#    check_rec_dir_gen_fields(demo_obj)
-#    check_rec_dir_gen_files(demo_obj)
-
-###################
-# RUN AND WRAP UP #
-###################
-
-def run():
-    """ Run this file. """
-    test()
-
-if __name__ == "__main__":
-    run()
