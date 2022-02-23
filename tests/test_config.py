8f262099cde14af7e5a4fe7d1ff0fbe8b8963738
tomhosker <tomdothosker@gmail.com>
2022-02-23 17:25:03 +0000

    Debugging...

--- tests/test_config.py
+++ tests/test_config.py
@@ -43,7 +43,7 @@ def test_custom_json():
     new_path_to_output = "/something/else"
     test_config_json_path = "test_config.json"
     test_config_json_str = (
-        "{ 'general': { 'path_to_output': '"+new_path_to_output+"' } }"
+        '{ "general": { "path_to_output": "'+new_path_to_output+'" } }'
     )
     with open(test_config_json_path, "w") as test_config_json_file:
         test_config_json_file.write(test_config_json_str)
