765b7b33a7e12e47f1cc746b9a8aacb47044246d
tomhosker <tomdothosker@gmail.com>
2022-02-24 13:37:55 +0000

    Added code coverage checks.

--- validate.py
+++ validate.py
@@ -5,13 +5,31 @@ This code defines a script which in turn runs pytest on our source code.
 # Non-standard imports.
 import pytest
 
+# Local constants.
+DEFAULT_MIN_CODE_COVERAGE = 50
+DEFAULT_MIN_LINT_SCORE = 8
+
+#############
+# FUNCTIONS #
+#############
+
+def run_tests(min_code_coverage=DEFAULT_MIN_CODE_COVERAGE):
+    """ Run PyTest. """
+    arguments = [
+        "--cov-report", "term",
+        "--cov-fail-under="+str(min_code_coverage),
+        "--cov=."
+    ]
+    return_code = pytest.main(arguments)
+    assert return_code == 0, "PyTest returned code: "+str(return_code)
+
 ###################
 # RUN AND WRAP UP #
 ###################
 
 def run():
     """ Run this file. """
-    pytest.main()
+    run_tests()
 
 if __name__ == "__main__":
     run()
