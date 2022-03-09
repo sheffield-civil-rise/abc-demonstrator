132d83b1bd16dd39a0abd5ce96224d89b85cef92
tomhosker <tomdothosker@gmail.com>
2022-03-09 21:03:00 +0000

    Debugging...

--- tests/test_demonstrator.py
+++ tests/test_demonstrator.py
@@ -30,7 +30,7 @@ def demo_obj():
     result = \
         Demonstrator(
             path_to_input_override=CONFIGS.paths.path_to_test_input,
-            debug=True
+            debug=False
         )
     result.demonstrate()
     return result
